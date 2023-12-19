df64199e5b16e15ed922abf90379b8da879ddb53
Franmalas <fran.malasquez@gmail.com>
2023-12-19 06:38:46 -0500

    Agregammos el codigo para imprimir un arbol

--- file_test.py
+++ file_test.py
@@ -4,3 +4,9 @@ x = np.array(range(1,10))
 
 for i in x:
     print("The number is:",i)
+
+y = "*"
+s = " "
+h = int(input("De cuantas lineas quieres tu arbol?:"))
+for i in range(1, h + 1):
+    print(s* (h - i) + y * (2 * i - 1))
